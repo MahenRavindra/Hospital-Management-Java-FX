e77911058414124d9c5c7cfef4b531f07a80fbe9
MaheRavindra <24ravindraicrox@gmail.com>
2022-09-17 21:33:08 +0530

    Update SignUpFormController.java
    2

--- src/Code/controller/SignUpFormController.java
+++ src/Code/controller/SignUpFormController.java
@@ -1,8 +1,20 @@
 package Code.controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class SignUpFormController {
-    public void backToLoginOnAction(ActionEvent actionEvent) {
+    public AnchorPane SignUPFormContext;
+
+    public void backToLoginOnAction(ActionEvent actionEvent) throws IOException {
+        Stage stage= (Stage) SignUPFormContext.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource(
+                "../view/LoginForm.fxml"
+        ))));
     }
 }
