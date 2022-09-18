50f9483e8c4e4f96dc0279168ca7e2b08096ab7d
MaheRavindra <24ravindraicrox@gmail.com>
2022-09-18 14:32:47 +0530

    ui 2

--- src/Code/controller/DashBoardController.java
+++ src/Code/controller/DashBoardController.java
@@ -1,6 +1,7 @@
 package Code.controller;
 
 import com.sun.deploy.util.FXLoader;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.AnchorPane;
 
@@ -15,9 +16,17 @@ public class DashBoardController {
     }
 
     private void loadDefaultData() throws IOException {
+        setUI("DefaultForm");
+    }
+
+    public void openDoctorManageOnAction(ActionEvent actionEvent) throws IOException {
+        setUI("DoctorForm");
+    }
+
+    private void setUI(String location) throws IOException {
         dashboardContext.getChildren().clear();
         dashboardContext.getChildren().add(
-                FXMLLoader.load(getClass().getResource("../view/DefaultForm.fxml"))
+                FXMLLoader.load(getClass().getResource("../view/"+location+".fxml"))
         );
     }
 }
---
+++ src/Code/controller/DoctorFormController.java
@@ -0,0 +1,15 @@
+package Code.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
+public class DoctorFormController {
+    public TextField txtid;
+    public TextField txtname;
+    public TextField txtaddress;
+    public TextField txtsalary;
+
+    public void saveDoctorOnAction(ActionEvent actionEvent) {
+
+    }
+}
